261917234eb2623a0790ac4e7f1ca573711ddc1c
kartikr <kartikr@adobe.com>
2023-11-01 13:02:08 +0530

    add rate-limiter lld problem

---
+++ rate-limiter/src/main/java/com/github/lld-java-deck/App.java
@@ -0,0 +1,13 @@
+package com.github.lld-java-deck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
