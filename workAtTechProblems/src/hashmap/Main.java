54c8fe18bfb78f82df26c79a3bbe066f0103563b
kartikr <kartikr@adobe.com>
2025-03-05 23:41:55 +0530

    add comments

--- workAtTechProblems/src/hashmap/Main.java
+++ workAtTechProblems/src/hashmap/Main.java
@@ -1,5 +1,12 @@
 package hashmap;
 
+/**
+ * Implementation -> https://www.youtube.com/watch?v=AsAymWn7D40
+ * defaultSize -> 16 (Always as a power of 2 i.e. 2^n to ensure better collision management)
+ * loadFactor -> 0.75 i.e. if the hashMap if 75% full, then it's maybe time to resize the hashMap
+ * In Java 8 there is tree fy, which means that if the linked list size is greater than 8 then convert it into a balanced binary search tree
+ * to make it fast
+ */
 public class Main {
     public static void main(String[] args) {
         IMap<String, Integer> map = new HashMapImplV2<String, Integer>();
