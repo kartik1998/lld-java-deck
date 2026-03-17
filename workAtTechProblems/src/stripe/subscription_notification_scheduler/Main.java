5d7fce25120d469394151ed64fd202e152c7758a
kartikr <kartikr@adobe.com>
2026-03-17 12:32:25 +0530

    add q

--- workAtTechProblems/src/stripe/subscription_notification_scheduler/Main.java
+++ workAtTechProblems/src/stripe/subscription_notification_scheduler/Main.java
@@ -1,4 +1,15 @@
 package stripe.subscription_notification_scheduler;
 
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        NotifcationManager manager = new NotifcationManager(
+                List.of(new UserAccount("Alice", "Pro", 1, 30),
+                        new UserAccount("Bob", "Basic", 10, 10)),
+                Map.of("start", "Start", "-7", "7 Days left", "-1", "1 Day left", "end", "Expired")
+        );
+        manager.listNotifications();
+    }
 }
